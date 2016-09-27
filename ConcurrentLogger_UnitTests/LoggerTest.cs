af154e81fb06154f9fa3756079443019214394db
mbv <terehovk@ya.ru>
2016-09-27 12:02:14 +0300

    Implementation test for UDP

--- ConcurrentLogger_UnitTests/LoggerTest.cs
+++ ConcurrentLogger_UnitTests/LoggerTest.cs
@@ -48,5 +48,23 @@ namespace ConcurrentLogger_UnitTests
             CollectionAssert.AreEqual(Encoding.Default.GetBytes(stringBuilder.ToString()),
                 secondFakeTargetStream.GetMessage());
         }
+
+        [TestMethod]
+        public void TestUdpTarget()
+        {
+            int bufferLimit = 5,
+                logsCount = 100;
+            var udpServer = new TestUdpServer("127.0.0.1", 9000);
+            var targetUdp = new UdpLoggerTarget("127.0.0.1", 9000, "127.0.0.1", 10000);
+            StringBuilder stringBuilder = new StringBuilder();
+            udpServer.StartReceive();
+            var logsCreator = new LogsFactory(new Logger(bufferLimit, new ILoggerTarget[] {targetUdp}));
+            logsCreator.Create(logsCount, LogLevel.Debug);
+            for (int i = 0; i < logsCount; i++)
+                stringBuilder.Append(i);
+            udpServer.Synchronize();
+            udpServer.Close();
+            CollectionAssert.AreEqual(Encoding.Default.GetBytes(stringBuilder.ToString()), udpServer.GetMessage());
+        }
     }
 }
